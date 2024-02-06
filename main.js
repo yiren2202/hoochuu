ecb0622ea8c41dd6367ca29478e52e19cd3ca936
yiren2202 <yiren2202@gmail.com>
2024-02-06 15:27:44 +0800

    preload images

--- main.js
+++ main.js
@@ -4,12 +4,22 @@ let lastKnownScrollPosition = 0;
 let ticking = false;
 let innerW = window.innerWidth;
 let cutebag__img_index = 1;
+let cutebag__img_total = 117;
 let scrollPos__temp;
 // Map function to convert the range
 function mapRange(value, inMin, inMax, outMin, outMax) {
     return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
- }
-
+}
+// preload any image
+function preload_image(im_url) {
+  let img = new Image();
+  img.src = im_url;
+}
+for(let i=1; i<=cutebag__img_total; i++)
+{
+  console.log(i);
+  preload_image("img/0125Test/running"+i+".png");
+}
 function doSomething(scrollPos) {
   // 依據捲動位置進行某些操作
   console.log(scrollPos);
@@ -17,7 +27,7 @@ function doSomething(scrollPos) {
   document.getElementById("cutebag").style.left = mapRange(scrollPos, 0, innerW*0.7, 0+innerW*0.5, innerW-innerW*0.01)+ "px";
   if(scrollPos-scrollPos__temp > 0)
   {
-    if(cutebag__img_index <117)
+    if(cutebag__img_index < cutebag__img_total)
     {
         cutebag__img_index++;
     }
@@ -32,7 +42,7 @@ function doSomething(scrollPos) {
         cutebag__img_index--;
     }
     else{
-        cutebag__img_index = 117;
+        cutebag__img_index = cutebag__img_total;
     }
   }
   scrollPos__temp = scrollPos;
