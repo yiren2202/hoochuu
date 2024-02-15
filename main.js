63681377dadb80b30422fe33b2d6b239e6302719
yiren2202 <yiren2202@gmail.com>
2024-02-15 16:37:59 +0800

    test goToAndStop

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ for(let i=1; i<=cutebag__img_total; i++)
 }
 function doSomething(scrollPos) {
   // 依據捲動位置進行某些操作
-  console.log(scrollPos);
+  // console.log(scrollPos);
   let bagleft = 0;
   if(scrollPos <= scrollPos__check1)
   {
@@ -34,7 +34,7 @@ function doSomething(scrollPos) {
   else if(scrollPos <= scrollPos__check2)
   {
     bagleft = innerW*0.9 - innerW*0.4*((scrollPos-scrollPos__check1)/(scrollPos__check2-scrollPos__check1));
-    console.log(bagleft);
+    // console.log(bagleft);
     document.getElementById("cutebag").style.left = bagleft+"px";
   }
   else
@@ -65,6 +65,8 @@ function doSomething(scrollPos) {
   }
   scrollPos__temp = scrollPos;
 //   cutebag__img_index = scrollPos%117 + 1;
+  console.log(cutebag__img_index);
+  animation1.goToAndStop(cutebag__img_index+10, true);
   document.getElementById("cutebag__img").src = "img/0126_compressed/running"+cutebag__img_index+".png";
 }
 
@@ -81,32 +83,42 @@ document.addEventListener("scroll", function (e) {
   }
 });
 
-let player1 = document.getElementById("lottie1");
-let player2 = document.getElementById("lottie2");
+let lottie1 = document.getElementById("lottie1");
+// let lottie2 = document.getElementById("lottie2");
+let animation1;
+// let animation2;
 
-player1.addEventListener("ready", () => {
-  LottieInteractivity.create({
-			  mode:"scroll",
-			  player: "#lottie1",
-			  actions: [
-        {
-            visibility:[0, 1.0],
-            type: "seek",
-            frames: [0, 300],
-        },
-        ]
-			});
+animation1 = lottie.loadAnimation({
+  container: lottie1, // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: false,
+  path: 'https://assets5.lottiefiles.com/packages/lf20_FISfBK.json' // the path to the animation json
 });
-player2.addEventListener("ready", () => {
-  LottieInteractivity.create({
-			  mode:"scroll",
-			  player: "#lottie2",
-			  actions: [
-        {
-            visibility:[0, 1.0],
-            type: "seek",
-            frames: [0, 300],
-        },
-        ]
-			});
-});
+
+// player1.addEventListener("ready", () => {
+//   LottieInteractivity.create({
+// 			  mode:"scroll",
+// 			  player: "#lottie1",
+// 			  actions: [
+//         {
+//             visibility:[0, 1.0],
+//             type: "seek",
+//             frames: [0, 300],
+//         },
+//         ]
+// 			});
+// });
+// player2.addEventListener("ready", () => {
+//   LottieInteractivity.create({
+// 			  mode:"scroll",
+// 			  player: "#lottie2",
+// 			  actions: [
+//         {
+//             visibility:[0, 1.0],
+//             type: "seek",
+//             frames: [0, 300],
+//         },
+//         ]
+// 			});
+// });
