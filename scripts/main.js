61566cbb1551f8fb2f3915dc031fb31dce3519a2
yiren2202 <yiren2202@gmail.com>
2024-03-15 23:03:27 +0800

    fix style

--- scripts/main.js
+++ scripts/main.js
@@ -181,7 +181,7 @@ function doSomething(scrollPos) {
     }
     else
     {
-      bagtop = innerH*-0.12 + innerH*0.2*((scrollPos-scrollPos__check5)/(scrollPos__bottom-scrollPos__check5)) ;
+      bagtop = innerH*-0.12 + innerH*0.1*((scrollPos-scrollPos__check5)/(scrollPos__bottom-scrollPos__check5)) ;
     }
   }
   else if(innerW >= 768) {
